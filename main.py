87fdfe78f558b3fcfcfc1bb5e9d04239d1c7d1a3
João Déda <joaoluispoletto@gmail.com>
2025-09-22 00:33:53 -0300

    teste docker

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 import random
+from pydantic import BaseModel
 app = FastAPI()
+
+class Estudante(BaseModel):
+    name: str 
+    curso: str
+    ativo: bool
 
 
 @app.get("/")
@@ -15,4 +21,15 @@ async def root():
 async def funcaoteste():
     return{"teste": True, "NumAleatorio": random.randint(0, 20000)}
 
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
 
